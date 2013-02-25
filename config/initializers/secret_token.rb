ffdc710081b2733c9d07b56e8b943c24c0b85505
Bence Erdos <bence.erdos@gmail.com>
2013-02-25 21:51:50 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcsNet::Application.config.secret_token = 'e15ddbbe297ea1fef47f3e4a768b02f85a91ce2324cf03748cc77e52d83b3a8dc4f32e88c3d46365e350c40f95d8c38414bda35c7a37072dc11d79fb9be3833d'
