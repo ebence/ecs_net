a45387bc40215f025a8f01c5728f4d670eb2c133
Bence Erdos <bence.erdos@gmail.com>
2013-02-26 14:01:30 +0100

    Default host url added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ EcsNet::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => ENV['SENDGRID_DOMAIN']}
   config.action_mailer.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
