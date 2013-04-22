5753bdfe0d64782aeed5571551561517289ec077
Csanad <erdos@ecs-solutions.hu>
2013-04-22 23:00:18 +0200

    try 9

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,7 +2,7 @@ class PrivateController < ApplicationController
   before_filter :authenticate_user!
 
   def serve
-    send_data(File.read(File.join(Rails.root,"/app/assets#{request.fullpath}")))
+    send_data(File.read(File.join(Rails.root,"/assets#{request.fullpath}")))
   #puts "#{Rails.root}#{request.fullpath}"
   #render :nothing => true
   end
