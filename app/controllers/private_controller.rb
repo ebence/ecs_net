2d02e79191aed130f0c3d978c14114a8f088b962
Csanad <erdos@ecs-solutions.hu>
2013-04-22 21:48:24 +0200

    try 2

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,8 +2,8 @@ class PrivateController < ApplicationController
   before_filter :authenticate_user!
 
   def serve
-    send_file("#{Rails.root}#{request.fullpath}")
-    #puts "#{Rails.root}#{request.fullpath}"
+    send_file("#{Rails.root}#{request.fullpath}", :x_sendfile => true)
+    puts "#{Rails.root}#{request.fullpath}"
     #render :nothing => true 
   end
 
