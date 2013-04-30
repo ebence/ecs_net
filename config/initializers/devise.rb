1881bf748dec345351e3573317f9d97ed023dc6e
Csanad <erdos@ecs-solutions.hu>
2013-04-30 09:37:45 +0200

    Translations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "no-reply@ecs-net.com"
+  config.mailer_sender = "no-reply@ecs-solutions.hu"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
